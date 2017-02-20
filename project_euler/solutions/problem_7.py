d0e10d2c6bdb3f616a4e087b1b5dccab90520049
Henk-Jaap Wagenaar <wagenaarhenkjaap@gmail.com>
2017-02-20 20:34:12 +0000

    Get rid of defense programming in problems

--- project_euler/solutions/problem_7.py
+++ project_euler/solutions/problem_7.py
@@ -2,9 +2,6 @@ from ..library.sequences import primes_sequence as primes
 
 
 def solve(number: int=10001) -> str:
-    if number <= 0:
-        raise ValueError
-
     for i, prime in enumerate(primes()):
         if i + 1 == number:
             return str(prime)
