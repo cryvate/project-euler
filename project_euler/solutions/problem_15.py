47abbc4a106744cd101e0a7a2f08410ff2410802
Henk-Jaap Wagenaar <wagenaarhenkjaap@gmail.com>
2017-04-02 23:06:20 +0100

    Clear up unnecessary str calls.

--- project_euler/solutions/problem_15.py
+++ project_euler/solutions/problem_15.py
@@ -1,6 +1,6 @@
 from math import factorial
 
 
-def solve(rows: int=20, columns: int=20) -> str:
+def solve(rows: int=20, columns: int=20) -> int:
     combs = factorial(rows + columns) // (factorial(rows) * factorial(columns))
-    return str(combs)
+    return combs
