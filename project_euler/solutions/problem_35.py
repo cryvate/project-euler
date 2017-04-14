77ad159b260d793e2856faafdbdcb2a1a105dc61
Henk-Jaap Wagenaar <wagenaarhenkjaap@gmail.com>
2017-04-14 23:59:25 +0800

    Improve problem 35

--- project_euler/solutions/problem_35.py
+++ project_euler/solutions/problem_35.py
@@ -1,20 +1,22 @@
 from typing import List
 
 
-from ..library.number_theory.primes import prime_sieve
+from ..library.sqrt import fsqrt
+from ..library.number_theory.primes import is_prime, prime_sieve
 
 
 def is_circular_prime(n: int, sieve: List[int]) -> bool:
-    # assume prime
-
-    for i in range(1, len(str(n))):
-        if int(str(n)[i:] + str(n)[:i]) not in sieve:
+    for i in range(len(str(n))):
+        if not is_prime(int(str(n)[i:] + str(n)[:i]), sieve):
             return False
+
+    print(n)
 
     return True
 
 
 def solve(digits: int=6) -> int:
-    sieve = prime_sieve(10 ** digits)
+    maximum = 10 ** digits
+    sieve = prime_sieve(fsqrt(maximum))
 
-    return sum(1 for prime in sieve if is_circular_prime(prime, sieve))
+    return sum(1 for prime in range(maximum) if is_circular_prime(prime, sieve))
