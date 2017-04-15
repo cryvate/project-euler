230443d3e035252f7f1d56c556f8ae2b701a41cc
Henk-Jaap Wagenaar <wagenaarhenkjaap@gmail.com>
2017-04-15 15:00:05 +0800

    Improve problem 27

--- project_euler/solutions/problem_27.py
+++ project_euler/solutions/problem_27.py
@@ -1,28 +1,28 @@
 from itertools import count
 
-from ..library.number_theory.primes import is_prime
+from ..library.number_theory.primes import is_prime, prime_sieve
 
 
 def consecutive(a: int, b: int, sieve) -> int:
-    for n in count():
+    # assume b prime
+
+    for n in count(1):
         if not is_prime(n ** 2 + a * n + b, sieve):
             return n
 
 
-def solve(a_bound: int=1_000, b_bound: int=1_000, sieve_bound: int=100) -> int:
+def solve(a_bound: int=1_000, b_bound: int=1_000, sieve_bound: int=10_00) -> int:
     maximum = 0
-    a_max = 0
-    b_max = 0
+    result = 0
 
-    sieve = [n for n in range(sieve_bound) if is_prime(n)]
+    primes = prime_sieve(sieve_bound)
 
     for a in range(-a_bound, a_bound + 1):
-        for b in range(-b_bound, b_bound + 1):
-            present = consecutive(a, b, sieve)
+        for b in primes:
+            present = consecutive(a, b, primes)
 
             if present > maximum:
                 maximum = present
-                a_max = a
-                b_max = b
+                result = a * b
 
-    return a_max * b_max
+    return result
