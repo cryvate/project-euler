9cb23ff36a19ea2579fce7ef4114e3b20bd542cb
Henk-Jaap Wagenaar <wagenaarhenkjaap@gmail.com>
2017-04-16 21:48:12 +0800

    Make solve console script and test better and more consistent

--- project_euler/solutions/solve.py
+++ project_euler/solutions/solve.py
@@ -12,23 +12,17 @@ Options:
 '''
 
 from importlib import import_module
+import os
+import time
+import warnings
 
 from docopt import docopt
-
-from project_euler.solutions.test_solutions import test_problems
+import yaml
 
 import project_euler.solutions  # noqa: F401
+from project_euler.solutions.problems import slow_numbers as slow_problems
 
-
-def solve(problem_number: int) -> str:
-    try:
-        problem_module = import_module(f'.problem_{problem_number}',
-                                       package='project_euler.solutions')
-    except ModuleNotFoundError as e:
-        raise SolveException(f"The problem {problem_number} does not seem to "
-                             "have a solution in this package.") from e
-
-    return problem_module.solve()
+spec = '{:4.2f}'
 
 
 class SolutionWrong(Exception):
@@ -61,19 +55,85 @@ class SolveException(Exception):
     pass
 
 
+def solve_problem(problem_number: int) -> str:
+
+    filename = os.path.join(os.path.split(__file__)[0],
+                            f'problem_{problem_number}.yaml')
+    try:
+        with open(filename) as f:
+            parameters = yaml.load(f)
+    except FileNotFoundError as e:
+        raise ProblemMalformed(f'File problem_{problem_number}.yaml not '
+                               f'found.') from e
+
+    if "title" not in parameters:
+        raise ProblemMalformed(f'No title in problem {problem_number}.')
+    if "description" not in parameters:
+        raise ProblemMalformed(f'No description in problem {problem_number}.')
+
+    if "answer_b64" in parameters:
+        if "strategy" not in parameters:
+            raise ProblemMalformed(f'No strategy in problem {problem_number} '
+                                   f'while providing answer.')
+
+        reference_answer = parameters['answer_b64'].decode()
+
+        start = time.time()
+        answer = str(solve(problem_number))  # often more natural to return int
+        spent = time.time() - start
+
+        kwargs = {
+            'answer': answer,
+            'reference_answer': reference_answer,
+            'spent': spent
+        }
+
+        reference_answer = parameters['answer_b64'].decode()
+
+        if answer != reference_answer:
+            raise AnswerVerificationFailed(
+                f'In problem {problem_number} the calculated answer is '
+                f'{answer} ({spec.format(spent)}s), the reference answer is '
+                f'{reference_answer}.', **kwargs)
+
+        print(f'Problem {problem_number} took {spec.format(spent)}s and the '
+              f'answer {answer} was correct.')
+
+        if spent > 60:
+            if problem_number in slow_problems:
+                slower_time = slow_problems[problem_number]
+                if spent > slower_time:
+                    raise OneMinuteRuleViolation(
+                        f'Problem {problem_number} took {spec.format(spent)}s,'
+                        f' which is more than the {slower_time}s it is '
+                        f'allowed to take.', **kwargs)
+                else:
+                    warnings.warn(
+                        f'Problem {problem_number} took {spec.format(spent)}s,'
+                        f' which is less than the {slower_time}s it is allowed'
+                        f' to take, but more than 60s',
+                        UserWarning)
+            else:
+                raise OneMinuteRuleViolation(
+                    f'Problem {problem_number} took {spec.format(spent)}s, '
+                    f'which is more than a minute!', **kwargs)
+
+    return answer, spent
+
+
+def solve(problem_number: int) -> str:
+    try:
+        problem_module = import_module(f'.problem_{problem_number}',
+                                       package='project_euler.solutions')
+    except ModuleNotFoundError as e:
+        raise SolveException(f"The problem {problem_number} does not seem to "
+                             "have a solution in this package.") from e
+
+    return problem_module.solve()
+
 if __name__ == '__main__':
     arguments = docopt(__doc__)
 
     problem_number = arguments['<problem_number>']
 
-    spec = '{:4.2f}'
-
-    try:
-        result, spent = test_problems(problem_number)
-    except SolutionWrong as e:
-        print(f'Implemented answer: {e.answer} (in {spec.format(e.spent)}s)')
-        print(f'Reference solution: {e.reference_answer}')
-        print(f'This does *NOT* agree with reference answer.')
-    else:
-        print(f'Solution: {result} (in {spec.format(spent)}s)')
-        print('This *does* agree with reference answer.')
+    solve_problem(problem_number)
