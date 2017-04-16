2d9b545a3bc315cc498d1b8bbe6cda0582f7cd1e
Henk-Jaap Wagenaar <wagenaarhenkjaap@gmail.com>
2017-04-16 23:17:35 +0800

    Add base and u se that to massively increase speed/simplicity 34/74

--- project_euler/library/test_base.py
+++ project_euler/library/test_base.py
@@ -1,8 +1,9 @@
 import pytest
 
-from typing import List
+from typing import List, Tuple
 
-from .base import number_to_list, list_to_number, is_permutation
+from .base import number_to_list, list_to_number, is_permutation, \
+    range_combinations_digits, count_combinations_digits, count_permutations
 
 
 NUMBER_TO_LIST = [(2, [2], 10), (12, [1, 2], 10), (6, [2, 0], 3)]
@@ -13,6 +14,29 @@ PERMUTATIONS = [
     (12, 10, 2, True),
     (4, 3, 2, False),
     (15, 5000, 10, False),
+]
+
+RANGE_COMBINATIONS = [
+    (
+        [0, 2],
+        [()] + [(i,) for i in range(1, 10)]
+    )
+]
+
+COUNT_COMBINATIONS = [
+    (
+        [1],
+        8,
+        [(i,) for i in range(1, 8)]
+    )
+]
+
+COMBINATION_CONTRIBUTIONS = [
+    (
+        [1, 2, 3, 3],
+        True,
+        21
+    )
 ]
 
 
@@ -36,3 +60,32 @@ def test_is_permutation(self: int,
                         base: int,
                         expected_output: bool) -> None:
     assert is_permutation(self, other, base) == expected_output
+
+
+@pytest.mark.parametrize('args,expected_output', RANGE_COMBINATIONS)
+def test_range_combinations_digits(args, expected_output) -> None:
+    assert list(range_combinations_digits(*args)) == expected_output
+
+
+@pytest.mark.parametrize('args,max_value,expected_output', COUNT_COMBINATIONS)
+def test_count_combinations_digits(args: List[int],
+                                   max_value: int,
+                                   expected_output: List[Tuple[int]]) -> None:
+    result = []
+    for value in count_combinations_digits(*args):
+        print(value)
+        if list_to_number(value) >= max_value:
+            break
+
+        result.append(value)
+
+    assert result == expected_output
+
+
+@pytest.mark.parametrize('combination,one_also_zero_not_leading,contribution',
+                         COMBINATION_CONTRIBUTIONS)
+def test_count_permutations(combination: Tuple[int],
+                            one_also_zero_not_leading: bool,
+                            contribution: int) -> None:
+    assert count_permutations(combination, one_also_zero_not_leading) == \
+           contribution
